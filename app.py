caeb65794fedf6761a5142f8a42c33d7068a59af
Jay Ravaliya <jayrav13@gmail.com>
2017-03-06 21:20:48 -0500

    Removing legacy files

--- app.py
+++
@@ -1,9 +0,0 @@
-from lib.unemployment import Unemployment
-import json
-
-unemployment = Unemployment()
-data = unemployment.scrape()
-
-f = open('./data/test.json', 'w')
-f.write(json.dumps(data, indent=4, sort_keys=True))
-f.close()
--- lib/unemployment/__init__.py
+++
@@ -1,1 +0,0 @@
-from .unemployment import Unemployment
